e6155cdf38e1f3628075b4163a74da9963db4637
Lucas Cravero <ljcravero@gmail.com>
2022-11-26 17:15:33 -0300

    feat: Import all example components in parent App component

--- react-app-coderhouse/src/App.js
+++ react-app-coderhouse/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import Counter from "./components/Counter/Counter";
+// import Greeting from "./components/Greeting/Greeting";
+// import Greeting2 from "./components/Greeting2/Greeting2";
+// import Greeting4 from "./components/Greeting4/Greeting4";
+import ProductsList from "./components/ProductsList/ProductsList";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <Counter /> */}
+      {/* <Greeting /> */}
+      {/* <Greeting2 /> */}
+      {/* <Greeting4 /> Class component */}
+      <ProductsList />
     </div>
   );
 }
